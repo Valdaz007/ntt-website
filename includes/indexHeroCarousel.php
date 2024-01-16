371e7d74d10133ca2ef5cff8e0df36aa11c2db73
Victor <volsavaivictor@gmail.com>
2024-01-16 13:28:05 +1000

    Mobile View Okay

--- includes/indexHeroCarousel.php
+++ includes/indexHeroCarousel.php
@@ -25,49 +25,51 @@
                     </div>
                     
                     <!-- Slide 2 -->
-                    <div class="carousel-item c-slide" data-bs-interval="7000">
+                    <div class="carousel-item c-slide" data-bs-interval="70000">
                         <div class="slide-content">
                             <div class="s-content-inner">
-                                <div class="s-plans-heading p-2">
-                                    <h1 class="hero-slide2-head">BUSINESS EMAILS</h1>
+
+                                <div class="slide-content-heading slide2 p-2">
+                                    <h1>BUSINESS EMAILS</h1>
                                     <h3>Easy to use and secure! </h3>
                                 </div>
-                                <div class="row s2-plans">
-                                    <div class="col-6 col-md-4 mail p-1">
-                                        <div class="mail-box">
-                                            <div class="mail-img"></div>
-                                            <div class="mail-txt">
-                                                <p style="padding: 5px; text-align: center;">
-                                                    Suitable for individual professionals and small businesses.
-                                                </p>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="col-6 col-md-4 mail p-1">
-                                        <div class="mail-box">
-                                            <div class="mail-img"></div>
-                                            <div class="mail-txt">
+
+                                <div class="mail-plans">
+                                    <div class="mail-box">
+                                        <div class="mail-img mail-img1"></div>
+                                        <div class="mail-txt">
                                             <p style="padding: 5px; text-align: center;">
-                                                Take a step further with additional features that advance your email experience.
+                                                Suitable for individual professionals and small 
+                                                businesses.
                                             </p>
-                                            </div>
                                         </div>
                                     </div>
-                                    <div class="col-0 col-md-4"></div>
+
+                                    <div class="mail-box">
+                                        <div class="mail-img mail-img2"></div>
+                                        <div class="mail-txt">
+                                            <p style="padding: 5px; text-align: center;">
+                                                Take a step further with additional features that 
+                                                advance your email experience.
+                                            </p>
+                                        </div>
+                                    </div>
                                 </div>
+
                             </div>
                         </div>
                     </div>
 
                     <!-- Slide 3 -->
-                    <div class="carousel-item c-slide" data-bs-interval="7000">
+                    <div class="carousel-item c-slide" data-bs-interval="70000">
                         <div class="slide-content">
-                            <div class="row s-content-inner col-12">
-                                <div class="col-md-5 hero-texts">
+                            <div class="s-content-inner">
+                                <div class="slide-content-heading">
                                     <h1>VSAT BROADBAND INTERNET</h1>
                                     <h3>Suitable for Remote Locations</h3>
                                 </div>
-                                <div class="col-md-7 hero-plans">
+                                <div class="vsat-img">
+                                    <img src="./static/img/data-plans/vsat-data-plans.png" alt="">
                                 </div>
                             </div>
                         </div>
